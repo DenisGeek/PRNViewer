344636d738db110f5631878e9b054bb16391fbbb
Denis Pavlovish Anokhin <dpa@orggeophysical.no>
2020-05-04 19:26:28 +0200

    Add project files.

---
+++ PRNViewer/RegexMaskGroupColName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRNViewer
+{
+    class RegexMaskGroupColName
+    {
+        public string Name { get; set; }
+        public string Mask { get; set; }
+        
+        private string[] _headers = null;
+        private string[][] _data = null;
+        public string[] Groups { get; }
+
+
+    }
+}
