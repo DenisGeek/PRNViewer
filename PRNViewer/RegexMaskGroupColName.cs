3ac4d6464a6c9a9e40811709e070a6aea11ae1de
Denis Anokhin <mail@denis.tk>
2020-05-04 22:14:03 +0200

    Test git

--- PRNViewer/RegexMaskGroupColName.cs
+++ PRNViewer/RegexMaskGroupColName.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PRNViewer
 {
     class RegexMaskGroupColName
     {
         public string Name { get; set; }
-        public string Mask { get; set; }
+        
+        private string _maskString;
+        private Regex _maskRegex = new Regex("", RegexOptions.Compiled);
+        public string Mask 
+        {
+            get => _maskString;
+            set
+            {
+                _maskString = value;
+                _maskRegex = new Regex(value, RegexOptions.Compiled);
+            }
+        }
         
         private string[] _headers = null;
         private string[][] _data = null;
-        public string[] Groups { get; }
+        
+        public RegexMaskGroupColName(string[] Headers, string[][] Data)
+        {
+            _headers = Headers;
+            _data = Data;
+        }
+
+        public List<(string, int)> Group()
+        {
+            var res = new List<(string, int)>();
+
+            return res;
+        }
+
 
 
     }
